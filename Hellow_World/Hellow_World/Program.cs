73a03d2946f2d678fb2e30ba299e5260fcfd8bfc
俊华 蔡 <1032031912@qq.com>
2019-09-14 03:30:06 +0800

    添加了加法

--- Hellow_World/Hellow_World/Program.cs
+++ Hellow_World/Hellow_World/Program.cs
@@ -8,6 +8,11 @@ namespace Hellow_World
         {
             //项目注释
             Console.WriteLine("Hello World!");
+            //加法运算
+            int a = 1;
+            int b = 2;
+            //结果
+            Console.WriteLine("a+b=" + (a + b).ToString());
             Console.ReadLine();
         }
     }

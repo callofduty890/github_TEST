48ffe82b5d873da569e1a0a11cee80dac19ea6fc
俊华 蔡 <1032031912@qq.com>
2019-09-14 03:22:08 +0800

    提交新内容

---
+++ Hellow_World/Hellow_World/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hellow_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //项目注释
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
